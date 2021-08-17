c7a1899c568d9c8b8ae33f8b6617e4022390e4bd
Richa-rani02 <49235266+Richa-rani02@users.noreply.github.com>
2021-08-17 21:20:25 +0530

    Feat:code added

---
+++ app.js
@@ -0,0 +1,28 @@
+var inputText = document.querySelector("#input-txt");
+var translateButton = document.querySelector("#btn-translate");
+var outputtext = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+//console.log(getTranslationURL("I am richa"));
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+function clickHandler() {
+    var textInput = inputText.value;
+
+
+    fetch(getTranslationURL(textInput))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputtext.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
+
+translateButton.addEventListener("click", clickHandler);
